65590f4fd48822445e5a7cd17ca2f25c06b73e5f
Santosh <santoshsan3685@gmail.com>
2025-12-30 16:42:27 +0530

    Add client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,46 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/lib/theme-context";
+import { Navigation } from "@/components/navigation";
+import { Footer } from "@/components/footer";
+import Home from "@/pages/home";
+import Shop from "@/pages/shop";
+import Collections from "@/pages/collections";
+import About from "@/pages/about";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/shop" component={Shop} />
+      <Route path="/collections" component={Collections} />
+      <Route path="/about" component={About} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <TooltipProvider>
+          <div className="min-h-screen flex flex-col">
+            <Navigation />
+            <div className="flex-1">
+              <Router />
+            </div>
+            <Footer />
+          </div>
+          <Toaster />
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
